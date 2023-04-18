35c899ecb1bb08f70974117759ec3861fe936ede
Faleti Oluwaseyi <faletioluwaseyisam@gmail.com>
2023-04-18 23:48:48 +0100

    first commit

---
+++ src/main/java/mist_safe/mistsafe/MistsafeApplication.java
@@ -0,0 +1,13 @@
+package mist_safe.mistsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MistsafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mist_safe/mistsafe/MistsafeApplicationTests.java
@@ -0,0 +1,13 @@
+package mist_safe.mistsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistsafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
